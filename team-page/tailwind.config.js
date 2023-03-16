97cdce1ff042c9ba1e2bd9b06a4487e69e806eb3
uwais-ix <uzi241299@gmail.com>
2023-03-16 22:06:34 +0400

    the-team-page + updated tailwind starter npm scr

---
+++ team-page/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        ptSefif: ['PT Serif', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
